1b9e9ee30cedf581509235db48c15abbef79fb0c
Danish Pruthi <ddanish@devfair036.maas>
2019-06-03 09:54:55 -0700

    updates for smoother setup to new machines

--- install_script.sh
+++ install_script.sh
@@ -2,7 +2,6 @@ cd .vim/bundle/
 git clone https://github.com/scrooloose/syntastic
 git clone https://github.com/altercation/vim-colors-solarized
 git clone https://github.com/lervag/vimtex
-git clone https://github.com/Valloric/YouCompleteMe
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/NLKNguyen/papercolor-theme.git
