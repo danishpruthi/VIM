d15b85cfb5f5b3328cfc29e02596a54a580becae
Danish <danish037@gmail.com>
2020-09-29 21:45:40 -0400

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,12 +1,12 @@
 cd .vim/bundle/
 git clone https://github.com/scrooloose/syntastic
 git clone https://github.com/altercation/vim-colors-solarized
-git clone https://github.com/lervag/vimtex
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/NLKNguyen/papercolor-theme.git
 git clone https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/morhetz/gruvbox.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # install YouCompleteMe
 cd YouCompleteMe
